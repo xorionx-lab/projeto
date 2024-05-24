085fb523f20629b446a19da5466682983af5d25e
unknown <dennis.grupoaffectio@gmail.com>
2024-05-24 16:30:37 -0300

    agora

--- tela.py
+++ tela.py
@@ -1,3 +1,14 @@
 import streamlit as st
 from imc import *
 
+def cadastro():
+    contador = 0
+    while contador < 3:
+        nome = st.text_input("Digite o nome para o cadastro: ", key=f"nome{contador}")
+        senha = st.text_input("Digite uam senha para o cadastro:", type="password", key=f"senha{contador}")
+        csenha = st.text_input("Confirme a senha:", type="password", key=f"csenha{contador}")
+
+    if st.button("Cadastrar",key=f"Cadastrar{contador}"):
+        if senha == csenha:
+            st.success(f"Seja bem vindo: {nome}")
+            break
